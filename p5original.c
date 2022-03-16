01aac9665ee05cfc99cd12b9c94930e007850441
Madhavi S <97089896+madhavis2002@users.noreply.github.com>
2022-03-16 04:33:20 +0000

    points form a triangle

---
+++ p5original.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int input_array_size()
+{
+  int n;
+  printf("enter the number");
+  scanf("%d",&n);
+  return n;
+}
+void init_array(int n,int a[n])
+{
+  int i;
+  for(i=0;i<n;i++)
+    {
+      a[i]=i+1;
+    }
+}
+void erotosthenes_sieve(int n,int a[n])
+{
+  int i,j;
+  for(i=1;i<n;i++)
+}
